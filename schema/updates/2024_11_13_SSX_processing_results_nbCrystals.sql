30bd36d8712c41ee1d5ce275bf15bd63a0eb737c
Alberto Nardella <alberto.nardella@maxiv.lu.se>
2024-11-13 14:34:57 +0100

    new column in SSXProcessingResult table

--- schema/updates/2024_11_13_SSX_processing_results_nbCrystals.sql
+++ schema/updates/2024_11_13_SSX_processing_results_nbCrystals.sql
@@ -1,6 +1,6 @@
 INSERT INTO SchemaStatus (scriptName, schemaStatus) VALUES ('2024_11_13_SSX_processing_results_nbCrystals.sql', 'ONGOING');
 
-ALTER TABLE ispyb_build.SSXProcessingResult ADD nbCrystals INT NULL COMMENT 'Number of crystals';
+ALTER TABLE SSXProcessingResult ADD nbCrystals INT NULL COMMENT 'Number of crystals';
 
 UPDATE SchemaStatus SET schemaStatus = 'DONE' WHERE scriptName = '2024_11_13_SSX_processing_results_nbCrystals.sql';
 
